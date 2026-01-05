30f4c97a8d3316315848024b3f8652bd792ec224
Shah Samiur Rahman <sam@Shahs-MacBook-Air.local>
2026-01-05 06:54:18 -0800

    Fix bug by updating subnet values and rds backup retention period

--- locals.tf
+++ locals.tf
@@ -12,15 +12,15 @@ locals {
 
   name_prefix = "${local.project_name}-${local.environment}"
   availability_zones = local.environment == "dev" ? ["${local.region}a", "${local.region}b"] : ["${local.region}a", "${local.region}b", "${local.region}c"]
-  public_subnet_cidrs = local.environment == "dev" ? ["10.10.1.0/24", "10.10.2.0/24"] : ["10.20.1.0/24", "10.20.2.0/24", "10.20.3.0/24"]
-  private_subnet_cidrs = local.environment == "dev" ? ["10.10.11.0/24", "10.10.21.0/24"] : ["10.20.11.0/24", "10.20.21.0/24", "10.20.31.0/24"]
-  database_subnet_cidrs = local.environment == "dev" ? ["10.10.12.0/24", "10.10.22.0/24"] : ["10.20.12.0/24", "10.20.22.0/24", "10.20.32.0/24"]
+  public_subnet_cidrs = local.environment == "dev" ? ["10.0.1.0/24", "10.0.2.0/24"] : ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  private_subnet_cidrs = local.environment == "dev" ? ["10.0.11.0/24", "10.0.21.0/24"] : ["10.0.11.0/24", "10.0.21.0/24", "10.0.31.0/24"]
+  database_subnet_cidrs = local.environment == "dev" ? ["10.0.12.0/24", "10.0.22.0/24"] : ["10.0.12.0/24", "10.0.22.0/24", "10.0.32.0/24"]
   client_instance_type = local.environment == "dev" ? "t3.micro" : "t3.medium"
   server_instance_type = local.environment == "dev" ? "t3.micro" : "t3.medium"
   single_nat_gateway = local.environment == "dev" ? true : false
   one_nat_gateway_per_az  = local.environment == "dev" ? false : true
   map_public_ip_on_launch = local.environment == "dev" ? true : false
   multi_az = local.environment == "dev" ? false : true
-  backup_retention_period = local.environment == "dev" ? 7 : 35
+  backup_retention_period = local.environment == "dev" ? 0 : 35
   skip_final_snapshot = local.environment == "dev" ? true : false
 }
